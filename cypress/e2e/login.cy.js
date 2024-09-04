c5995bb0b301cd72494fc553e8df1e9a062e328b
viniciushpex <viniciuspereirahenrique@gmail.com>
2024-09-04 01:44:27 -0300

    Pequenas correções de ci

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,16 @@
 describe('login', () => {
   
+  //Executar o login com um usuário standard
   it('Fazer login como standard', () => {
     cy.fazerLogin('standard_user')
   })
 
+  //Executar o login com um usuário standard
   it('Fazer login com usuário bloqueado', () => {
 
     cy.fazerLogin('locked_out_user')
     cy.get('[data-test="error"]')
-      .contains('Epic saadface: Sorry, this user has been locked out.')
+      .contains('Epic sadface: Sorry, this user has been locked out.')
     cy.get('[data-test="error-button"]')
       .click()
   })
